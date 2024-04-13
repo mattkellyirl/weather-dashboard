8f853fdd3e8672d9d247e63d0e66b293b7216370
mattkellyirl <matthew.kelly@digicom.net.au>
2024-04-13 16:55:26 +0930

    restructured fetch to async

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-const locationContainer = document.querySelector(".location-container");
 const form = document.getElementById("search-form");
 const input = document.querySelector(".form-input");
 const title = document.getElementById("city");
@@ -18,26 +17,27 @@ form.addEventListener("submit", (e) => {
   getWeatherData(city);
 });
 
-// Get weather data from API
-const getWeatherData = (city) => {
+// Fetch weather data from API
+const getWeatherData = async (city) => {
   const apiKey = "4a1cc10d5b1e470064b3abd2776f656a";
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
-  fetch(apiUrl)
-    .then((res) => res.json())
-    .then((data) => {
-      if (data.cod === 200) {
-        displayWeather(data);
-      } else {
-        // locationContainer.innerHTML = `<p>${data.message}</p>`;
-      }
-    })
-    .catch((error) => {
-      console.error("Error fetching weather data:", error);
-      locationContainer.innerHTML =
-        "<p>Failed to fetch weather data. Please try again later.</p>";
-    });
+  try {
+    const response = await fetch(apiUrl);
+
+    if (response.status === 200) {
+      const data = await response.json();
+      console.log("Fetch Successful");
+      displayWeather(data);
+    } else {
+      throw new Error("Fetch Failed");
+    }
+  } catch (error) {
+    console.error("Fetch Failed", error.message);
+    throw error;
+  }
 };
+
 // Display weather for chosen city
 const displayWeather = (city) => {
   const tempData = city.main.temp;
